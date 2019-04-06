808d674b82464cd38729ea1304029b5c9838f5a2
AnteBiljan <ante.biljan1@gmail.com>
2019-04-06 15:28:55 +0200

    console game

--- main.cpp
+++ main.cpp
@@ -1,28 +1,96 @@
 #include <iostream>
 #include <string>
 
+class Player
+{
+
+public:
+	Player(int positionX, int positionY, int currentVelocitiy, int hitPoints)
+	{
+		x = positionX;
+		y = positionY;
+		velocity = currentVelocitiy;
+		hp = hitPoints;
+	}
+
+	int x;
+	int y;
+	int velocity;
+	int hp;
+	int strength;
+	std::string playerName;
+
+	void move()
+	{
+		x += velocity;
+		y += velocity;
+	}
+
+	void damage(int ammount)
+	{
+		hp -= ammount;
+	}
+};
+
+void move(int& x, int& y, int& velocity)
+{
+	x += velocity;
+	y += velocity;
+}
 
 int main()
 {
-	int number = 5;
-	int& tracker = number;
-	int* ptr = &number;
-	std::string* strptr;
-	std::string someWord = "Nesto";
-	strptr = &someWord;
-	int** doubleptr = &ptr;
+	Player playerOne = Player(0, 0, 10, 30);
+	playerOne.playerName = "Chaos";
+	playerOne.strength = 12;
 
-	number = 9;
-	std::cout << "Hello World" << std::endl;
-	std::cout << "number: " << number << std::endl;
-	std::cout << "tracker: " << tracker << std::endl;
-	std::cout << "pointer: " << ptr << std::endl;
-	std::cout << "strpointer: " << strptr << std::endl;
-	std::cout << "derefference strptr: " << *strptr << std::endl;
-	*strptr = "nesto drugo";
-	std::cout << "derefference strptr take 2: " << *strptr << std::endl;
-	std::cout << "doubleptr ptr address: " << *doubleptr << std::endl;
-	std::cout << "doubleptr ptr address: " << **doubleptr << std::endl;
+	Player playerTwo = Player(10, 15, 10, 30);
+	playerTwo.playerName = "robi256";
+	playerTwo.strength = 11;
 
+	playerOne.move();
+	playerTwo.move();
+	std::string losingPlayer;
+	std::string winPlayer;
+
+	while (playerOne.hp > 0 && playerTwo.hp > 0)
+	{
+		int check;
+		std::cout << "Choose player you want to hit :";
+		std::cin >> check;
+		std::cout << std::endl;
+		std::cin.ignore(1000, '\n');
+
+		switch (check)
+		{
+		case 1:
+			playerOne.damage(playerTwo.strength);
+			std::cout << "Player one lost 5 hitpoits" << std::endl;
+			break;
+		case 2:
+			playerTwo.damage(playerOne.strength);
+			std::cout << "Player two lost 5 hitpoits" << std::endl;
+			break;
+		default:
+			break;
+		}
+
+		std::cout << "Player one hitpoints: " << playerOne.hp << std::endl;
+		std::cout << "Player two hitpoints: " << playerTwo.hp << std::endl;
+
+		if (playerOne.hp <= 0)
+		{
+			losingPlayer = playerOne.playerName;
+			winPlayer = playerTwo.playerName;
+		}
+		else if (playerTwo.hp <= 0)
+		{
+			losingPlayer = playerTwo.playerName;
+			winPlayer = playerOne.playerName;
+		}
+	}
+
+	std::cout << winPlayer << " wins!" << std::endl;
+	std::cout << losingPlayer << " lost" << std::endl;
 	std::cin.get();
 }
