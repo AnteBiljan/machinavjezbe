8a8cee5a52aabbc2a95fbaeca30c9f115b24ae0b
ante_biljan <ante.biljan1@gmail.com>
2019-05-01 16:45:53 +0200

    Persons task

--- main.cpp
+++ main.cpp
@@ -1,51 +1,124 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-
-struct Vector3
-{
-	float x, y, z;
-
-};
-
-class Entity
+class Person
 {
 public:
-	Vector3 position;
-
-protected:
-	std::string name;
-};
-
-class Player : public Entity
-{
-public:
-	Vector3 getPosition()
+	Person(std::string name, std::string lastName, std::string address)
 	{
-		return position;
+		_name = name;
+		_lastName = lastName;
+		_address = address;
+	}
+	bool trySetSSN(std::string ssn)
+	{
+		if (_socialSecurityNumber != "")
+		{
+			std::cout << "Social Security Number is already set!" << std::endl;
+			std::cout << "Persons SSN cannot be changed!" << std::endl;
+			return true;
+		}
+		if (ssn.size() != 11)
+		{
+			std::cout << "Social Security Number is invalid! Wrong number of characters!" << std::endl;
+			return false;
+		}
+
+		for (size_t i = 0; i < ssn.size(); i++)
+		{
+			char current = ssn[i];
+			if (current < 48 || current > 57)
+			{
+				std::cout << "Social Security Number is invalid!" << std::endl;
+				std::cout << "Character at index " << i << " is invalid" << std::endl;
+				return false;
+			}
+		}
+
+		_socialSecurityNumber = ssn;
+		return true;
 	}
 
-	std::string getName()
+	void print()
 	{
-		return name;
+		std::cout << "Name: " << _name << " " << _lastName << std::endl;
+		std::cout << "Address: " << _address  << std::endl;
+		std::cout << "Social Security Number: " << _socialSecurityNumber << std::endl;
 	}
 
+private:
+	std::string _name, _lastName, _address, _socialSecurityNumber;
 };
 
-class Enemy : public Entity
+void displayPersons(std::vector<Person> persons)
 {
-public:
+	for (size_t i = 0; i < persons.size(); i++)
+	{
+		persons[i].print();
+	}
+}
 
-};
+void inputNewData(std::string message, std::string& data)
+{
+	std::cout << message << std::endl;
+	std::cin >> data;
+	std::cin.clear();
+	std::cin.ignore(1000, '\n');
+}
+
+void createPerson(std::vector<Person>& persons)
+{
+	std::string name, lastName, address;
+	inputNewData("Enter new persons name: ", name);
+	inputNewData("Enter new persons lastname: ", lastName);
+	inputNewData("Enter new persons address: ", address);
+
+	Person person = Person(name, lastName, address);
+	
+	while (true)
+	{
+		std::string ssn;
+		inputNewData("Enter new social security number: ", ssn);
+
+		if (person.trySetSSN(ssn))
+		{
+			persons.push_back(person);
+			return;
+		}
+	}
+}
 
 int main()
 {
-	Player player = Player();
-	Enemy enemy = Enemy();
 
-	player.position;
-	enemy.position;
+	std::vector<Person> persons;
 
-	std::cout << player.getName() << std::endl;
+	bool valid = true;
+	while (valid)
+	{
+		std::cout << "What do you want to do: " << std::endl;
+		std::cout << "1 - Add new person" << std::endl;
+		std::cout << "2 - Exit" << std::endl;
+
+		int choice;
+		std::cin >> choice;
+		std::cin.clear();
+		std::cin.ignore(1000, '\n');
+
+		switch (choice)
+		{
+		case 1:
+			createPerson(persons);
+			break;
+		case 2:
+			valid = false;
+			break;
+		default:
+			break;
+		}
+	}
+	displayPersons(persons);
+
 	std::cin.get();
 }
