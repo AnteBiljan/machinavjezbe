dc7a38189c326510152136b58d56ac080367a749
ante_biljan <ante.biljan1@gmail.com>
2019-05-01 14:02:38 +0200

    class inheritance

--- main.cpp
+++ main.cpp
@@ -1,116 +1,51 @@
 #include <iostream>
 #include <string>
 
-class Player
+
+struct Vector3
 {
+	float x, y, z;
 
-public:
-	Player(int positionX, int positionY, int currentVelocitiy, int hitPoints)
-	{
-		x = positionX;
-		y = positionY;
-		velocity = currentVelocitiy;
-		hp = hitPoints;
-	}
-
-	int x;
-	int y;
-	int velocity;
-	int hp;
-	int strength;
-	std::string playerName;
-
-	void move()
-	{
-		x += velocity;
-		y += velocity;
-	}
-
-	void damage(int ammount)
-	{
-		hp -= ammount;
-	}
-
-	std::string getPrivateString()
-	{
-		return _privateString;
-	}
-
-	void setPrivateString(std::string stringParam)
-	{
-		_privateString = stringParam;
-	}
-private:
-	std::string _privateString;
-
-protected:
-	std::string _protectedString;
 };
 
-void move(int& x, int& y, int& velocity)
+class Entity
 {
-	x += velocity;
-	y += velocity;
-}
+public:
+	Vector3 position;
+
+protected:
+	std::string name;
+};
+
+class Player : public Entity
+{
+public:
+	Vector3 getPosition()
+	{
+		return position;
+	}
+
+	std::string getName()
+	{
+		return name;
+	}
+
+};
+
+class Enemy : public Entity
+{
+public:
+
+};
 
 int main()
 {
-	Player playerOne = Player(0, 0, 10, 30);
-	playerOne.playerName = "Chaos";
-	playerOne.strength = 12;
+	Player player = Player();
+	Enemy enemy = Enemy();
 
-	Player playerTwo = Player(10, 15, 10, 30);
-	playerTwo.playerName = "robi256";
-	playerTwo.strength = 11;
+	player.position;
+	enemy.position;
 
-	playerOne.move();
-	playerTwo.move();
-
-	playerOne.setPrivateString("Ja sam privatan");
-
-	std::string losingPlayer;
-	std::string winPlayer;
-
-	std::cout << playerOne.getPrivateString() << std::endl;
-
-	while (playerOne.hp > 0 && playerTwo.hp > 0)
-	{
-		int check;
-		std::cout << "Choose player you want to hit :";
-		std::cin >> check;
-		std::cout << std::endl;
-		std::cin.ignore(1000, '\n');
-
-		switch (check)
-		{
-		case 1:
-			playerOne.damage(playerTwo.strength);
-			std::cout << "Player one lost 5 hitpoits" << std::endl;
-			break;
-		case 2:
-			playerTwo.damage(playerOne.strength);
-			std::cout << "Player two lost 5 hitpoits" << std::endl;
-			break;
-		default:
-			break;
-		}
-
-		std::cout << "Player one hitpoints: " << playerOne.hp << std::endl;
-		std::cout << "Player two hitpoints: " << playerTwo.hp << std::endl;
-
-		if (playerOne.hp <= 0)
-		{
-			losingPlayer = playerOne.playerName;
-			winPlayer = playerTwo.playerName;
-		}
-		else if (playerTwo.hp <= 0)
-		{
-			losingPlayer = playerTwo.playerName;
-			winPlayer = playerOne.playerName;
-		}
-	}
-
-	std::cout << winPlayer << " wins!" << std::endl;
-	std::cout << losingPlayer << " lost" << std::endl;
+	std::cout << player.getName() << std::endl;
 	std::cin.get();
 }
